53e2c01fb2559bb469a7a0510da9eea677eb593f
Terry Liao <terry.liao@nogle.com>
2021-12-10 12:48:39 +0800

    change to class

--- index.js
+++ index.js
@@ -2,69 +2,59 @@ require("dotenv").config();
 const ccxt = require("ccxt");
 const axios = require("axios");
 
-const app = require("express")();
-const PORT = process.env.PORT || 5000;
-
-const config = {
-  asset: "BTC",
-  base: "USDT",
-  allocation: 0.2, // how much for each trade in percentage from portfolio
-  spread: 0.003, // percentage that applies to the mid-rate to create buy/sell limit
-  tickInterval: 5000,
-};
-
 const binanceClient = new ccxt.binance({
   apiKey: process.env.API_KEY,
   secret: process.env.API_SECRET,
 });
 
-const tick = async (config, binanceClient) => {
-  const { asset, base, allocation, spread } = config;
-  const market = `${asset}/${base}`;
+class Bot {
+  constructor(asset, base, allocation, spread, interval = 2000) {
+    this.asset = asset;
+    this.base = base;
+    this.allocation = allocation;
+    this.spread = spread;
+    this.interval = interval;
+  }
 
-  const orders = await binanceClient.fetchOpenOrders(market);
-  console.log(orders.length);
-  // if (orders.length) {
-  //   return;
-  // }
-  // orders.forEach(async (order) => {
-  //   await binanceClient.cancelOrder(order.id, order.symbol);
-  // });
+  async tick(binanceClient) {
+    const market = `${this.asset}/${this.base}`;
+    const orders = await binanceClient.fetchOpenOrders(market);
 
-  const results = await axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT`);
-  const marketPrice = results.data.price;
-  console.log("marketPrice", marketPrice);
-  const sellPrice = marketPrice * (1 + spread);
-  const buyPrice = marketPrice * (1 - spread);
+    const results = await axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=${this.asset}${this.base}`);
+    const marketPrice = results.data.price;
+    this.showMarketPrice(marketPrice);
+    const sellPrice = marketPrice * (1 + this.spread);
+    const buyPrice = marketPrice * (1 - this.spread);
 
-  // get all balance in your wallet
-  const balance = await binanceClient.fetchBalance();
-  const assetBalance = balance.free[asset];
-  const baseBalance = balance.free[base];
-  const volume = (baseBalance * allocation) / marketPrice;
-  // const sellVolume = assetBalance * allocation;
+    // get all balance in your wallet
+    const balance = await binanceClient.fetchBalance();
+    const assetBalance = balance.free[this.asset];
+    const baseBalance = balance.free[this.base];
+    const volume = (baseBalance * this.allocation) / marketPrice;
+    // const sellVolume = assetBalance * allocation;
 
-  // await binanceClient.createLimitSellOrder(market, volume, sellPrice);
-  // await binanceClient.createLimitBuyOrder(market, volume, buyPrice);
+    // await binanceClient.createLimitSellOrder(market, volume, sellPrice);
+    // await binanceClient.createLimitBuyOrder(market, volume, buyPrice);
+    this.showTicketCreatedMessage(market, volume, sellPrice, buyPrice);
+  }
 
-  console.log(`
+  showMarketPrice(marketPrice) {
+    console.log(`Market price for ${this.asset}/${this.base}: ${marketPrice}`);
+  }
+  showTicketCreatedMessage(market, volume, sellPrice, buyPrice) {
+    console.log(`
     New tick for ${market}...
-    Created limit sell order for ${volume}@${sellPrice}
-    Created limit buy order for ${volume}@${buyPrice}
+    Created limit sell order for ${volume.toFixed(6)}${this.asset} @ ${sellPrice.toFixed(6)}${this.base}
+    Created limit buy order for ${volume.toFixed(6)}${this.asset} @ ${buyPrice.toFixed(6)}${this.base}
   `);
-};
+  }
+  run(binanceClient) {
+    this.tick(binanceClient);
+    setInterval(() => {
+      this.tick(binanceClient);
+    }, this.interval);
+  }
+}
 
-const run = (config, binanceClient) => {
-  tick(config, binanceClient);
-  setInterval(tick, config.tickInterval, config, binanceClient); // run tick function every 2 seconds
-};
-
-// express server
-app.get("/", (req, res) => {
-  res.send(`listening on port ${PORT}`);
-});
-
-app.listen(PORT, () => {
-  run(config, binanceClient);
-  console.log(`Server running on port ${PORT}`);
-});
+const bot1 = new Bot("BTC", "USDT", 0.2, 0.003);
+bot1.run(binanceClient);
